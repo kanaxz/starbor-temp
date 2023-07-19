0f6f0004f0de2a6d8999d8826ea2c01da5b1ca91
Albane-MAGNIN <magninalbane@yahoo.fr>
2023-07-19 17:24:06 +0200

    (feat) Implement new pages : MyProfile

---
+++ client/src/auth/pages/MyProfile/index.js
@@ -0,0 +1,26 @@
+const Page = require('hedera/page/Page')
+const Main = require('@app/layouts/Main')
+const template = require('./template.html')
+const auth = require('../../service')
+const navigator = require('@app/navigator')
+
+require('./style.scss')
+
+module.exports = class MyProfile extends Page {
+  constructor() {
+    super()
+    this.MyDataIsOpen = false
+    this.EditMyDataIsOpen = false
+  }
+}.define({
+  name: 'myprofile-page',
+  template,
+  layout: Main,
+})
+  .properties({
+    MyDataIsOpen: 'any',
+    EditMyDataIsOpen: 'any',
+  })
+  .variables({
+    auth,
+  })
